6787ff4821c838e38d6662f42872352ae8c3e294
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-03-11 16:09:53 -0300

    feat: Adicionando LivroService

---
+++ src/main/java/com/bn/exercicies/services/LivroService.java
@@ -0,0 +1,40 @@
+package com.bn.Exercicies.services;
+
+import com.bn.Exercicies.models.LivroModel;
+import com.bn.Exercicies.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    public List<LivroModel> findall(){
+        return livroRepository.findAll();
+    }
+
+    public Optional<LivroModel> findById(Long id){
+        return livroRepository.findById(id);
+    }
+
+    public LivroModel criarLivro(LivroModel livroModel){
+        return livroRepository.save(livroModel);
+    }
+
+    public LivroModel autalizarLivro(LivroModel livroModel, Long id){
+       LivroModel livro = livroRepository.findById(id).get();
+        livro.setTitulo(livroModel.getTitulo());
+
+        return livroRepository.save(livro);
+
+    }
+
+    public void deletarLivro(Long id){
+        livroRepository.deleteById(id);
+    }
+}
