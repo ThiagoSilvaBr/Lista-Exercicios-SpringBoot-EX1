d3a0ef3dcff3ce810a6998997d05826aaebc94bd
Thiago Henrique <164083408+ThiagoSilvaBr@users.noreply.github.com>
2026-03-17 16:06:05 -0300

    Ajustando o Controller

--- src/main/java/com/bn/exercicies/controllers/LivroController.java
+++ src/main/java/com/bn/exercicies/controllers/LivroController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/livraria")
+@RequestMapping("/livros")
 public class LivroController {
 
     @Autowired
@@ -40,7 +40,15 @@ public class LivroController {
         Optional<LivroModel> request = livroService.findById(id);
         return ResponseEntity.ok().body(request);
     }
-
+    
+    /*
+    @PutMapping("/{id}")
+    public ResponseEntity<LivroModel> atualizarLivro(@PathVariable Long id, @RequestBody LivroModel livroModel){
+        LivroModel request = livroService.atualizarLivro(id, livroModel);
+        return ResponseEntity.status(201).body(livroModel);
+    }
+    */
+    
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletarLivro(@PathVariable Long id){
         livroService.deletarLivro(id);
